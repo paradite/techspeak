932f15e4ee96a1182abbf4d46a005908f254bb08
Zhu Liang <zhuliangg11@gmail.com>
2018-11-03 00:01:07 +0800

    initial commit, get terms

---
+++ README.md
@@ -0,0 +1,11 @@
+# techspeak
+
+> Generate random sentences that contains tech words.
+
+[Live site]()
+
+## Tools and resources used
+
+* python
+* nltk
+* [stackapi](https://github.com/AWegnerGitHub/stackapi)
---
+++ get_terms.py
@@ -0,0 +1,20 @@
+import json
+import nltk
+from stackapi import StackAPI
+
+# setup nltk data first before running the code
+# python
+# >>> import nltk
+# >>> nltk.download()
+
+SITE = StackAPI('stackoverflow')
+SITE.page_size = 100
+SITE.max_pages = 2
+tags = [x['name'] for x in SITE.fetch('tags')['items'] if x['name'].isalpha()]
+tags_with_pos = nltk.tag.pos_tag(tags)
+
+with open('tags.json', 'w') as outfile:
+  json.dump(tags, outfile, indent=2)
+
+with open('tags_with_pos.json', 'w') as outfile:
+  json.dump(tags_with_pos, outfile, indent=2)
